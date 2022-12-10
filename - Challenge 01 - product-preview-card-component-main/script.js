3164da66ba097c30b1b2bd84b11063d33642e241
issam <aissam.16.25@gmail.com>
2022-12-10 08:20:53 +0100

    Add Desktop version

---
+++ - Challenge 01 - product-preview-card-component-main/script.js
@@ -0,0 +1,16 @@
+tailwind.config={
+    theme:{
+        extend:{
+            colors:{
+                "dark-cyan" : "hsl(158, 36%, 37%)",
+                "cream" : "hsl(30, 38%, 92%)",
+                "v-dark-blue" : "hsl(212, 21%, 14%)",
+                "dark-grayish-blue" : "hsl(228, 12%, 48%)",
+            },
+            fontFamily: {
+                'montserrat': 'Montserrat, sans-serif',
+                "fraunces" : ['Fraunces','serif']
+              }
+        }
+    }
+}
