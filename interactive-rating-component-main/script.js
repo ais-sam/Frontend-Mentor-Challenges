a2d2c815e83d9534e9a03943631ac3d2efeaf885
issam <aissam.16.25@gmail.com>
2022-12-14 07:55:15 +0100

    start challenge 02

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            orange : "hsl(25, 97%, 53%)",
+            "light-grey":"hsl(217, 12%, 63%)",
+            "med-grey" : "hsl(216, 12%, 54%)",
+            "dark-blue" : "hsl(213, 19%, 18%)",
+            "v-dark-blue" : "hsl(216, 12%, 8%)"
+        },
+        fontFamily: {
+            overpass: ['Overpass', 'sans-serif'],
+          },
+        
+      }
+    }
+  }
