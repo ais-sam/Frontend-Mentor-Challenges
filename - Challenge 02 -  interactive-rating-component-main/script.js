4b54d836958217547fa69cec3327b3811a2f45af
issam <aissam.16.25@gmail.com>
2022-12-16 22:25:29 +0100

    finish rating card design and add active state style

--- - Challenge 02 -  interactive-rating-component-main/script.js
+++ - Challenge 02 -  interactive-rating-component-main/script.js
@@ -5,12 +5,16 @@ tailwind.config = {
             orange : "hsl(25, 97%, 53%)",
             "light-grey":"hsl(217, 12%, 63%)",
             "med-grey" : "hsl(216, 12%, 54%)",
+            "light-blue" : "#303942",
             "dark-blue" : "hsl(213, 19%, 18%)",
             "v-dark-blue" : "hsl(216, 12%, 8%)"
         },
         fontFamily: {
             overpass: ['Overpass', 'sans-serif'],
           },
+        fontSize:{
+          "15" : "15px"
+        }
         
       }
     }
