0cdaf72dd9106b08a6f1caf9598f627bc9ff9266
issam <aissam.16.25@gmail.com>
2022-12-18 21:28:24 +0100

    start challenge 03

---
+++ - Challenge 03 -Qr-code-component/ script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "light-grey": "hsl(212, 45%, 89%)",
+          "grayish-blue" : "hsl(220, 15%, 55%)",
+          "dark-blue" : "hsl(218, 44%, 22%)"
+        },
+        fontFamily:{
+            "outfit" : ["Outfit", "sans-serif"]
+        }
+      }
+    }
+  }
